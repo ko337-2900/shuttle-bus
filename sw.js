5b5b41829d929a03f002e5e2ef2f26cdd60f4b36
ko337-2900 <art.together.2900.ko.337@gmail.com>
2026-06-26 19:27:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //   CACHE_VERSION を上げるだけで古いキャッシュが自動削除される
 // ─────────────────────────────────────────────────────────
 
-const CACHE_VERSION = 'v0.0.2131'; // ← 更新時にここだけ変更する
+const CACHE_VERSION = 'v0.0.2'; // ← 更新時にここだけ変更する
 const CACHE_NAME    = `shuttle-${CACHE_VERSION}`;
 
 // cache-first で管理する静的アセット
